4133868c5e78eb76bb3d0ed30773e51911dd5b1a
lance-go <lance.dinh.goldenowl@gmail.com>
2024-03-06 14:47:55 +0700

    Add screen

---
+++ lib/src/feature/recently_viewed/view/recently_viewed_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecentlyViewedScreen extends StatefulWidget {
+  const RecentlyViewedScreen({super.key});
+
+  @override
+  State<RecentlyViewedScreen> createState() => _RecentlyViewedScreenState();
+}
+
+class _RecentlyViewedScreenState extends State<RecentlyViewedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
