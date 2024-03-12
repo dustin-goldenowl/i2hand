e6df99b8c9924881cfdb39ebffe5a720dd51d41b
lance-go <lance.dinh.goldenowl@gmail.com>
2024-03-12 14:38:00 +0700

    Change folder

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -30,7 +30,7 @@ import 'package:i2hand/src/feature/home/view/home_screen.dart';
 import 'package:i2hand/src/feature/on_boarding/on_boarding_screen.dart';
 import 'package:i2hand/src/feature/product/logic/detail_product_bloc.dart';
 import 'package:i2hand/src/feature/product/view/detail_product_screen.dart';
-import 'package:i2hand/src/feature/profile/feature/setting/view/setting_screen.dart';
+import 'package:i2hand/src/feature/setting/view/setting_screen.dart';
 import 'package:i2hand/src/feature/profile/view/profile_screen.dart';
 import 'package:i2hand/src/feature/recently_viewed/logic/recently_viewed_bloc.dart';
 import 'package:i2hand/src/feature/recently_viewed/view/recently_viewed_screen.dart';
