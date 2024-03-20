041d86fbd4b02850dd8cdec563942ca1d2a6ee6b
lance-go <lance.dinh.goldenowl@gmail.com>
2024-03-20 14:16:08 +0700

    Change icon attributes and font style in DetailAProductScreen

--- lib/src/feature/product/widget/attributes_detail_widget.dart
+++ lib/src/feature/product/widget/attributes_detail_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:i2hand/gen/assets.gen.dart';
 import 'package:i2hand/src/config/enum/attribute_enum.dart';
 import 'package:i2hand/src/localization/localization_utils.dart';
+import 'package:i2hand/src/theme/colors.dart';
 import 'package:i2hand/src/theme/styles.dart';
 import 'package:i2hand/src/theme/value.dart';
 import 'package:i2hand/src/utils/padding_utils.dart';
@@ -18,43 +19,46 @@ class XAttributesDetailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        _getAttributeIcon(),
-        XPaddingUtils.horizontalPadding(width: AppPadding.p5),
-        _renderAttributeName(context),
-        Text(S.of(context).twoDots, style: AppTextStyle.contentTexStyle),
-        _renderAttributeValue(context),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: AppPadding.p5),
+      child: Row(
+        children: [
+          _getAttributeIcon(),
+          XPaddingUtils.horizontalPadding(width: AppPadding.p5),
+          _renderAttributeName(context),
+          Text(S.of(context).twoDots, style: AppTextStyle.contentTexStyle),
+          _renderAttributeValue(context),
+        ],
+      ),
     );
   }
 
   Widget _getAttributeIcon() {
     switch (attribute) {
       case AttributeEnum.status:
-        return Assets.svg.icStatus.svg(width: AppSize.s16);
+        return Assets.svg.icStatus.svg(width: AppSize.s24);
       case AttributeEnum.agencyPhone:
-        return Assets.svg.icLaptopAgency.svg(width: AppSize.s16);
+        return Assets.svg.icLaptopAgency.svg(width: AppSize.s24);
       case AttributeEnum.agencyLaptop:
-        return Assets.svg.icLaptopAgency.svg(width: AppSize.s16);
+        return Assets.svg.icLaptopAgency.svg(width: AppSize.s24);
       case AttributeEnum.microProcessor:
-        return Assets.svg.icMicroprocessor.svg(width: AppSize.s16);
+        return Assets.svg.icMicroprocessor.svg(width: AppSize.s24);
       case AttributeEnum.ram:
-        return Assets.svg.icRam.svg(width: AppSize.s16);
+        return Assets.svg.icRam.svg(width: AppSize.s24);
       case AttributeEnum.hardWare:
-        return Assets.svg.icHardware.svg(width: AppSize.s16);
+        return Assets.svg.icHardware.svg(width: AppSize.s24);
       case AttributeEnum.hardWareType:
-        return Assets.svg.icHardwareType.svg(width: AppSize.s16);
+        return Assets.svg.icHardwareType.svg(width: AppSize.s24);
       case AttributeEnum.graphicsCard:
-        return Assets.svg.icGraphicsCard.svg(width: AppSize.s16);
+        return Assets.svg.icGraphicsCard.svg(width: AppSize.s24);
       case AttributeEnum.screenSize:
-        return Assets.svg.icScreenSize.svg(width: AppSize.s16);
+        return Assets.svg.icScreenSize.svg(width: AppSize.s24);
       case AttributeEnum.warrantyPolicy:
-        return Assets.svg.icWarranty.svg(width: AppSize.s16);
+        return Assets.svg.icWarranty.svg(width: AppSize.s24);
       case AttributeEnum.origin:
-        return Assets.svg.icOrigin.svg(width: AppSize.s16);
+        return Assets.svg.icOrigin.svg(width: AppSize.s24);
       case AttributeEnum.color:
-        return Assets.svg.icColor.svg(width: AppSize.s16);
+        return Assets.svg.icColor.svg(width: AppSize.s24);
       default:
         return const SizedBox.shrink();
     }
@@ -63,14 +67,21 @@ class XAttributesDetailWidget extends StatelessWidget {
   Widget _renderAttributeName(BuildContext context) {
     return Text(
       attribute.getAttributeText(context),
-      style: AppTextStyle.contentTexStyle,
+      style: AppTextStyle.contentTexStyle.copyWith(
+        color: AppColors.black,
+        fontSize: AppFontSize.f15,
+        fontWeight: FontWeight.w500,
+      ),
     );
   }
 
   Widget _renderAttributeValue(BuildContext context) {
     return Text(
       attributeValue,
-      style: AppTextStyle.contentTexStyle,
+      style: AppTextStyle.contentTexStyle.copyWith(
+        color: AppColors.black,
+        fontSize: AppFontSize.f15,
+      ),
     );
   }
 }
